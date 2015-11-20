f15b49596c44c6a8a06776ab363ce64ff44a8d58
Shadowsun <wendyfreedom31@hotmail.com>
2015-11-20 14:38:50 +0100

    exo 7

---
+++ exo7/index.js
@@ -0,0 +1,34 @@
+// hello world dans une page web (server)
+var http = require('http');
+var url = require('url');
+var fs = require("fs");
+
+var routes = {
+  "/" : "index",
+  "/page1" : "page1",
+  "/page2" : "page2"
+}
+
+var server = http.createServer(function (request, response) {
+  console.log(request.url);
+  if(routes[request.url]){
+    response.writeHead(200, {"Content-Type": "text/html"});
+    var fileRoute = "templates/" + routes[request.url] + ".html";
+    fs.readFile(fileRoute , function(err, html){
+      if(err) throw err;
+      console.log(html);
+      response.end(html);
+    });
+  }else{
+      response.writeHead(404);
+      response.end("the URL : " + routes[request.url] + " doesn't exist");
+  }
+
+});
+
+server.listen(3000, function(err){
+  console.log("server is now listening on port 3000");
+});
+
+
+// url.resolve('/one/two/three', 'four')
